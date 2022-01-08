445c95c10d9f364544a86ab4d040da46c06edfc8
maitham h <maitham_gh98@hotmail.com>
2022-01-07 19:58:50 -0500

    1/7/2022 | Maitham | created a simple hello world application | 5 min

---
+++ src/Application.java
@@ -0,0 +1,21 @@
+/**
+ * A simple HelloWorld application. 
+ * @author Maitham Alghamgham
+ * @version 1/7/2022
+ * Assignment 0.1
+ * Spring 2022
+ */
+public class Application {
+
+	
+	/**
+	 * Main method to print out a simple Hello World!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}// end of main
+
+}// end of class
